95f3a067d59b9e4a5ca7c4b73e6e3683c84150a3
Foyzul Karim <Foyzul.Karim@Nintex.com>
2019-09-15 17:52:04 +0800

    Updates

---
+++ precache-manifest.50a3b1ea15aa24c0367bbe79246c3c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40051fdddc174d14bb68353482fbf7e5",
+    "url": "/memorizedquran/index.html"
+  },
+  {
+    "revision": "ce78abdd8f413b0f0924",
+    "url": "/memorizedquran/static/css/2.2cff08c1.chunk.css"
+  },
+  {
+    "revision": "82b1dffa97778dba6548",
+    "url": "/memorizedquran/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ce78abdd8f413b0f0924",
+    "url": "/memorizedquran/static/js/2.95d62067.chunk.js"
+  },
+  {
+    "revision": "82b1dffa97778dba6548",
+    "url": "/memorizedquran/static/js/main.e09c56ac.chunk.js"
+  },
+  {
+    "revision": "dde5b17e46afea584223",
+    "url": "/memorizedquran/static/js/runtime~main.bb927b8c.js"
+  }
+]);
